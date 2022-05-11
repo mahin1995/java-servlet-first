cd3e9b3f5664fb43bcf5586bf9fc64b2030d7442
mahin1995 <mohimanul8@gmail.com>
2022-05-12 03:26:21 +0600

    f

--- src/main/java/tags/TagHandaler.java
+++ src/main/java/tags/TagHandaler.java
@@ -1,5 +1,26 @@
 package tags;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
 
-public class TagHandaler {
+public class TagHandaler extends TagSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	public int doStartTag() {
+		try {
+			JspWriter out=pageContext.getOut();
+			out.println("<h1>This is my custom tag</h1>");
+		}catch(Exception e) {
+			
+		}
+		
+		return SKIP_BODY;
+		
+	}
+	
 
 }
